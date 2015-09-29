ec1956a3ba5dbd13a2c5f9de6d689c51c8c4af21
mike31s <michael88spencer@gmail.com>
2015-09-29 09:41:03 -1000

    updated for wod

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # greenjobs
-Work on jobs dataset
+Work on green jobs dataset
+
+# Overview
+
+Provides five functions with analytics to compute data for [green jobs] (http://philipmjohnson.github.io/ics314f15/morea/underscore/inclass-greenjobs.html)
+
+# Installation
+
+Provide the following scrpits in HTML files
+
+'''
+<script src="//philipmjohnson.github.io/ics314f15/morea/underscore/underscore-min.js"></script>
+<script src="//philipmjohnson.github.io/ics314f15/morea/underscore/greenjobs.js"></script>
+<script src="greenjobs.js"></script>
+'''
+
+#Usage
+
+Here are example calls:
+
+'''
+<script>
+  
+console.log(listIndustries(test_data))
+console.log(countyGreenJobs(test_data))
+console.log(jobswithKeyword(test_data, 'PV'))
+//console.log(totalDegrees(uhdata));</script>
+'''
+
+Consult greenjobs.js file for more details
+
+#Credits
+
+Uses the [Underscore] (http://underscorejs.org/) library
+Thanks to Josephine for providing [code](http://jsfiddle.net/jogarces/kp22pv8s/)
---
+++ greenjobs.js
@@ -0,0 +1,60 @@
+/**
+ * Created by mike on 9/29/2015.
+ */
+
+/**
+ *
+ * @type {Array.<T>}
+ */
+test_data = greenjobs.splice(0, 10);
+
+/**
+ * Lists industries in the dataset
+ * @param data the list of green jobs records
+ * @returns {*} An object with each Industry present
+ */
+function listIndustries(data) {
+  return _.uniq(_.pluck(data, 'Industry'))
+}
+
+/**
+ * Counts the jobs available in each county
+ * @param data the list of green jobs records
+ * @returns {*}  an object where the keys are County names and the values are the number of Green Jobs listed in that County
+ */
+function countyGreenJobs(data) {
+  return _.countBy(data, function (num) {
+    return num['County']
+  });
+}
+/**
+ *
+ * @param data the list of green jobs records
+ * @param keyword a job keyword to look for
+ * @returns {*} a list of Job Titles
+ */
+function jobswithKeyword(data, keyword) {
+  var jobs = _.filter(data, function (num) {
+    return num['Job Title'].indexOf(keyword) != -1;
+  });
+
+  return _.pluck(jobs, 'Job Title')
+}
+
+/**
+ * Lists job types and available positions
+ * @param data the list of green jobs records
+ * * @returns {*} an object with jobs and positions
+ */
+function industryJobs(data) {
+
+}
+
+/**
+ * Lists the industry and maximum amount of jobs available
+ * @param data the list of green jobs records
+ * @returns {*} a list of Job industries and openings
+ */
+function maxIndustryJobs(data) {
+
+}
